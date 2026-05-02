098fe526856cf34b257a2221380b397e04786536
burcinismail8 <burcinismail8@gmail.com>
2026-05-02 15:45:11 +0300

    Add HowItWorks component to Home page; include step-by-step guide with icons for enhanced user understanding and engagement.

---
+++ app/components/home/HowItWorks.tsx
@@ -0,0 +1,78 @@
+import Image from "next/image";
+
+const steps = [
+  {
+    id: 1,
+    icon: "/icons/research.svg",
+    title: "Открий",
+    text: "Намери най-добрите салони и специалисти в твоя град с няколко клика.",
+  },
+  {
+    id: 2,
+    icon: "/icons/calendar.svg",
+    title: "Резервирай",
+    text: "Избери удобен за теб час и запази мястото си веднага.",
+  },
+  {
+    id: 3,
+    icon: "/icons/check-mark.svg",
+    title: "Готово!",
+    text: "Остави всичко в ръцете на експертите и се потопи в заслуженото време за красота.",
+  },
+];
+
+const HowItWorks = () => {
+  return (
+    <section className="relative flex flex-col bg-[#FFB1DC] items-center justify-center gap-4 px-4 pb-10 pt-16 text-center font-yeseva-one sm:px-6 sm:pb-12 sm:pt-18 md:px-8 md:py-14 lg:px-10 lg:py-16 xl:px-12 xl:py-20 2xl:px-16">
+      {/* Decorative sparkle icon. It grows and moves out on wider screens. */}
+      <Image
+        src="/icons/shine-stars.svg"
+        alt="sparkles"
+        width={80}
+        height={80}
+        className="absolute left-4 top-4 h-10 w-10 sm:right-8 sm:top-2 sm:h-14 sm:w-14 md:right-10 md:top-6 md:h-16 md:w-16 lg:right-16 lg:h-20 lg:w-20 xl:right-24 2xl:right-32"
+      />
+
+      {/* Text block stays centered on mobile and gets roomier on desktop. */}
+      <div className="mb-5 flex flex-col items-center justify-center gap-3 sm:mb-6 md:mb-8 md:gap-4 lg:mb-10">
+        <h3 className="text-2xl text-white sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl">
+          Открий. Резервирай. Сияй.
+        </h3>
+        <div className="my-2 h-0.5 w-16 bg-white sm:w-20 md:my-3 md:w-24 lg:w-28 xl:w-32" />
+        <p className="text-sm leading-6 text-white sm:text-base  md:text-lg lg:text-lg 2xl:text-xl">
+          Всички салони и процедури на едно място. Бързо, лесно и винаги под
+          ръка.
+        </p>
+      </div>
+      <div className="grid w-full lg:w-3/4 gap-5 sm:grid-cols-3">
+        {steps.map((step) => {
+          return (
+            <div
+              key={step.id}
+              className="flex flex-col items-center justify-between rounded-3xl bg-white px-8 py-14 text-center text-[#FF70BF] shadow-lg/30 shadow-white"
+            >
+              <Image
+                src={step.icon}
+                alt={step.title}
+                width={80}
+                height={80}
+                className="mb-4 h-18 w-18"
+              />
+              <div className="mb-4 h-px w-2/3 bg-[#FF70BF]/35 sm:w-28 md:w-36 lg:w-44" />
+              <div className="min-h-24">
+                <h4 className="text-xl mb-3 leading-none sm:text-lg md:text-2xl lg:text-3xl text-[#E446A8]">
+                  {`${step.id}. ${step.title}`}
+                </h4>
+                <p className="mt-2 text-[#FF70BF] text-sm leading-5 sm:text-xs md:text-base lg:text-lg">
+                  {step.text}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default HowItWorks;
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import HeroSection from "./components/home/HeroSection";
 import BusinessCTA from "./components/home/BusinessCTA";
 import PopularCategories from "./components/home/PopularCategories";
+import HowItWorks from "./components/home/HowItWorks";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <PopularCategories />
+      <HowItWorks />
       <BusinessCTA />
     </div>
   );
