edfebf94e8950d225795b0f5f525fa010cb24fe1
burcinismail8 <burcinismail8@gmail.com>
2026-05-04 14:24:45 +0300

    Refactor SpecialistProfilePage layout; add BookingSection, PriceList, and Gallery components for improved organization and user experience.

--- app/[specId]/page.tsx
+++ app/[specId]/page.tsx
@@ -2,12 +2,24 @@
 import React from "react";
 import SpecialistCard from "../components/specialistPage/SpecialistCard";
 import LocationMap from "../components/specialistPage/LocationMap";
+import Gallery from "../components/specialistPage/Gallery";
+import PriceList from "../components/specialistPage/PriceList";
+import BookingSection from "../components/specialistPage/BookingSection";
 
 const SpecialistProfilePage = () => {
   return (
-    <div className="min-h-[800px] overflow-visible bg-linear-to-l from-[#FAD9EB] to-[#FFF8FD]">
-      <SpecialistCard />
-      <LocationMap />
+    <div className="min-h-[800px] overflow-visible bg-linear-to-l from-[#FAD9EB] to-[#FFF8FD] flex flex-col md:flex-row">
+      <div className="mr-6">
+        <SpecialistCard />
+        <LocationMap />
+      </div>
+      <div className="w-full">
+        <div className="flex flex-col md:flex-row">
+          <BookingSection />
+          <PriceList />
+        </div>
+        <Gallery />
+      </div>
     </div>
   );
 };
---
+++ app/components/specialistPage/BookingSection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BookingSection = () => {
+  return <div>BookingSection</div>;
+};
+
+export default BookingSection;
